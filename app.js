b79def79524eeaf1f1b5f7690e5e0466f959cf57
Khush Acharya <Khush Acharya>
2016-05-08 22:30:49 +0530

    First recruitment portal commit

---
+++ app.js
@@ -0,0 +1,67 @@
+﻿var express = require('express');
+var bodyparser = require('body-parser');
+var session = require('express-session');
+var hbs = require('express-handlebars');
+var chalk = require('chalk')
+
+var routes = require('./routes/routes.js');
+
+var app = express();
+
+app.use(express.static(__dirname + "/public"));
+
+app.use(bodyparser.json());
+app.use(bodyparser.urlencoded({ extended: false }));
+app.use(session({ secret: "secret", resave : true, saveUninitialized : false }));
+app.set('views', __dirname + '/views');
+app.set('view engine', 'handlebars');
+app.engine('handlebars', hbs({}));
+
+app.get('/', routes.loginPageHandler);
+app.get('/login', routes.loginPageHandler);
+app.get('/logout', routes.logoutPageHandler);
+app.post('/loginAuth', routes.loginAuthPageHandler);
+app.get('/refer', routes.referPageHandler);
+app.get('/hire', routes.hirePageHandler);
+app.get('/aboutus', routes.aboutusPageHandler);
+app.get('/contactus', routes.contactusPageHandler);
+app.get('/signup', routes.signupPageHandler);
+app.get('/account/', routes.accountHomePageHandler);
+app.get('/account/index', routes.accountHomePageHandler);
+app.get('/account/contact/manage', routes.manageContactPageHandler);
+app.get('/account/jobposting/view', routes.viewJobPostringPageHandler);
+app.get('/account/jobposting/new', routes.newJobPostringPageHandler);
+app.get('/account/resume/view', routes.viewResumePageHandler);
+app.get('/account/resume/new', routes.newResumePageHandler);
+app.get('/account/message/view', routes.viewMessagePageHandler);
+app.get('/account/message/new', routes.newMessagePageHandler);
+app.get('/account/notification/view', routes.viewNotificationPageHandler);
+app.get('/account/notification/new', routes.newNotificationPageHandler);
+app.get('/account/referral', routes.referralPageHandler);
+app.get('/account/reports', routes.reportsPageHandler);
+app.get('/account/reports/report1', routes.report1PageHandler);
+app.get('/account/reports/report2', routes.report2PageHandler);
+app.get('/account/reports/report3', routes.report3PageHandler);
+app.get('/account/profile', routes.profilePageHandler);
+app.get('/account/recruiterprofile', routes.recruitmentProfilePageHandler);
+
+////error handling
+//app.use("*", function (req, res) {
+//    res.status(404);
+//    res.render('message.handlebars', 
+//    	{ message: '<blockquote class="mainLines"><code> The page you are looking for is not available or may have been moved.</code> </blockquote>' }
+//    );
+//});
+
+//app.use(function (error, req, res, next) {
+//    console.log(chalk.red('Error : 500::' + error))
+//    res.status(500);
+//    res.render('message.handlebars', 
+//    	{ message: '<blockquote class="mainLines"><code>something went wrong as you tried to access this page</code>Probably this happened because there are some bugs in the application</blockquote>' }
+//    );
+//});
+
+var port = process.env.PORT || 5001;
+app.listen(port, function () {
+    console.log(chalk.green('HTTP server is listening on port: ' + port));
+});
---
+++ routes/routes.js
@@ -0,0 +1,97 @@
+﻿var chalk = require('chalk');
+
+exports.loginPageHandler = function (req, res) {
+    res.render('login.handlebars');
+};
+
+exports.loginAuthPageHandler = function (req, res) {
+    res.render('index.handlebars');
+};
+
+exports.referPageHandler = function (req, res) {
+    res.render('refer.handlebars');
+};
+exports.hirePageHandler = function (req, res) {
+    res.render('hire.handlebars');
+};
+exports.aboutusPageHandler = function (req, res) {
+    res.render('aboutus.handlebars');
+};
+exports.contactusPageHandler = function (req, res) {
+    res.render('contactus.handlebars');
+};
+exports.signupPageHandler = function (req, res) {
+    res.render('signup.handlebars');
+};
+
+exports.accountHomePageHandler = function (req, res) {
+    res.render('index.handlebars');
+};
+
+exports.manageContactPageHandler = function (req, res) {
+    res.render('contact/managecontacts.handlebars');
+};
+
+exports.viewJobPostringPageHandler = function (req, res) {
+    res.render('jobposting/viewjobposting.handlebars');
+};
+
+exports.newJobPostringPageHandler = function (req, res) {
+    res.render('jobposting/newjobposting.handlebars');
+};
+
+exports.newResumePageHandler = function (req, res) {
+    res.render('resume/newresume.handlebars');
+};
+exports.viewResumePageHandler = function (req, res) {
+    res.render('resume/viewresume.handlebars');
+};
+
+exports.viewMessagePageHandler = function (req, res) {
+    res.render('message/viewmessage.handlebars');
+};
+
+
+exports.newMessagePageHandler = function (req, res) {
+    res.render('message/newmessage.handlebars');
+};
+
+exports.viewNotificationPageHandler = function (req, res) {
+    res.render('notification/viewnotification.handlebars');
+};
+
+exports.newNotificationPageHandler = function (req, res) {
+    res.render('notification/newnotification.handlebars');
+};
+
+exports.referralPageHandler = function (req, res) {
+    res.render('referral/index.handlebars');
+};
+
+exports.reportsPageHandler = function (req, res) {
+    res.render('report/index.handlebars');
+};
+
+exports.report1PageHandler = function (req, res) {
+    res.render('report/report1.handlebars');
+};
+
+exports.report2PageHandler = function (req, res) {
+    res.render('report/report2.handlebars');
+};
+
+exports.report3PageHandler = function (req, res) {
+    res.render('report/report3.handlebars');
+};
+
+exports.profilePageHandler = function (req, res) {
+    res.render('profile/personalprofile.handlebars');
+};
+
+exports.recruitmentProfilePageHandler = function (req, res) {
+    res.render('profile/recruiterprofile.handlebars');
+};
+
+exports.logoutPageHandler = function (req, res) {
+    res.render('login.handlebars');
+};
